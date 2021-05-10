7c568551d8bf07177b909748d90aa3a85f2ca1f4
Mårten Jonsson <marten.jonsson90@hotmail.com>
2021-05-10 15:27:08 +0200

    Lagt med login samt skapa konto

--- app.py
+++ app.py
@@ -10,12 +10,24 @@ produkter.append(p)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="REKO")
+    return render_template("index.html")
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("sign_up.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
 
 # Sida för att visa vilka produkter man har, länkt till lägga till ny produkt, länk till uppdatera befintilig produkt
 @app.route("/manage_product", methods=["GET", "POST"])
 def manage_product():
+<<<<<<< HEAD
     return render_template("manage_product.html", products=produkter)
+=======
+    return render_template("manage_product.html", produkter=produkter)
+>>>>>>> f3ac4fe (Lagt med login samt skapa konto)
 
 
 @app.route("/add_product", methods=["GET", "POST"])
@@ -30,11 +42,7 @@ def add_product():
 #denna sida skall ta info från vald produkt och visa för justering
 @app.route("/update-product", methods=["GET", "POST"])
 def update_product():
-    return render_template("update_product.html", title="Uppdatera till produkt")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return render_template("login.html", title="Login")
+    return render_template("update_product.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
