67706cb226e0aed2710487fe14b64769fe51e959
August Aublet <august.aublet@gmail.com>
2021-05-15 18:05:28 +0200

    Laggt till session för att hålla reda på vilken användare som är aktiv. Även ändrat redirect i stället för render_tepmplate

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, redirect, session
 from Klasser.product import Product
-from testobjekt import produkter, rekoringar
-from testobjekt import producenter
+from testobjekt import produkter, rekoringar, användare, producenter
 import json
 
 app = Flask(__name__)
+app.secret_key="tesfaafefaca"
+current_user = None
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        user = request.form.get("user_select")
+        for a in användare:
+            name = a.get_name()
+            stripped = name.split(" ", 1)[0]
+            if user == stripped:
+                session["current_user"]= a.get_name()
+                return redirect(url_for("set_ring"))
+        for p in producenter:
+            name = p.get_name()
+            stripped = name.split(" ", 1)[0]
+            if user == stripped:
+                session["current_user"] = p.get_name()
+                return redirect(url_for("manage_product"))
+    return render_template("index.html", consumers=användare, producers=producenter)
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
@@ -21,14 +36,14 @@ def login():
 # Sida för att visa vilka produkter man har, länkt till lägga till ny produkt, länk till uppdatera befintilig produkt
 @app.route("/manage_product", methods=["GET", "POST"])
 def manage_product():
+
     return render_template("manage_product.html", products=produkter)
 
 @app.route("/set_ring", methods=["GET","POST"])
 def set_ring():
     if request.method == "POST":
-        current_ring = request.form.get("set_ring")
-        return render_template("consumer_shopping.html",products=produkter, producers=producenter, current_ring=current_ring)
-
+        session["current_ring"] = request.form.get("set_ring")
+        return redirect(url_for("consumer_shopping"))
     return render_template("set_ring.html", ringar = rekoringar)
 
 @app.route("/add_product", methods=["GET", "POST"])
@@ -40,7 +55,7 @@ def add_product():
         isForSale = request.form.get("isForSale")
         ny_produkt = Product(namn, pris,"Producent", description, isForSale)
         produkter.append(ny_produkt)
-        return render_template("manage_product.html", products=produkter)
+        return redirect(url_for("manage_product"))
 
     return render_template("add_product.html", producers = producenter)
 
@@ -52,11 +67,11 @@ def update_product():
 @app.route("/handla", methods=["GET", "POST"])
 def consumer_shopping():
     if request.method == "POST":
-        current_ring = request.form.get("set_ring")
-        return render_template("consumer_shopping.html",products=produkter, producers=producenter, current_ring=current_ring)
+        if request.form.get("set_ring"):
+            session["current_ring"] = request.form.get("set_ring")
+            return redirect(url_for("consumer_shopping"))
 
-    current_ring= "Välj en ring"
-    return render_template("consumer_shopping.html", products=produkter, producers=producenter, ringar=rekoringar, current_ring=current_ring)
+    return render_template("consumer_shopping.html", products=produkter, producers=producenter, ringar=rekoringar, current_ring=session["current_ring"])
 
 @app.route("/buy-product", methods=["POST"])
 def buy_product():
